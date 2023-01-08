af0cf73601afa6b8c5bb81b0150893905111de0a
BB-Simon <simon.chowdery@gmail.com>
2023-01-08 22:53:43 +0200

    variables, data types, string and string methods

---
+++ giraffe.rb
@@ -0,0 +1,65 @@
+# print "Hello world!"
+# print "Hello Simon!"
+# puts "Hi"
+# puts "Hello"
+# puts "Simon"
+
+# Shape
+# puts "     /|"
+# puts "    / |"
+# puts "   /  |"
+# puts "  /   |"
+# puts " /____|"
+
+
+# #################################################################
+# ######################## Variables In Ruby ######################
+# #################################################################
+
+# char_name = "Simon"
+# char_age = "30"
+
+# puts ("There once was a man named " + char_name)
+# puts ("Hi was " + char_age + " year old")
+# puts ("Hi really like the name " + char_name)
+# puts ("But did not like bing " + char_age)
+
+# #################################################################
+# ######################## Data Types In Ruby #####################
+# #################################################################
+
+name = 'Simon'
+occupation = 'Programmer'
+age = 30
+gpa = 3.45
+isNotMale = false
+ismaried = true
+flows = nil
+
+# #################################################################
+# ##################### Working with String In Ruby ###############
+# #################################################################
+
+# puts 'Soft  "Tech"'
+# puts "Soft  'Tech'"
+# puts "Soft  \"Tech\""
+# puts "Soft  \nTech"
+
+title = "Soft Tech"
+# title = "          Soft Tech         "
+# puts title.upcase()
+# puts title.downcase()
+# puts title.strip()
+# puts title.length()
+# puts title.include? "Soft"
+# puts title.include? "Hello"
+
+# Acces to string by index
+# puts title[6]
+
+# Acces a range
+# puts title[1, 3]
+
+# get index of a character
+puts title.index('t')
+
