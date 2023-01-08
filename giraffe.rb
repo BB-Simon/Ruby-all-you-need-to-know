a986e2e77d56fbcbffd618260d1369f21071e531
BB-Simon <simon.chowdery@gmail.com>
2023-01-09 00:52:22 +0200

    Number and Array in ruby

--- giraffe.rb
+++ giraffe.rb
@@ -61,5 +61,89 @@ title = "Soft Tech"
 # puts title[1, 3]
 
 # get index of a character
-puts title.index('t')
+# puts title.index('t')
 
+# #################################################################
+# ##################### Math and Number In Ruby ###############
+# #################################################################
+
+# num = 20.49
+# puts ("My fav nu7um is " + num.to_s)
+# puts num.abs()
+# puts num.round()
+
+# num2 = 2.1
+# puts num2.ceil()
+# puts num2.floor()
+
+# puts Math.sqrt(36)
+# puts Math.log(20)
+
+# puts 2.3 + 23
+# puts 2.3 / 23
+
+
+# #################################################################
+# ##################### Getting user input In Ruby ###############
+# #################################################################
+
+# puts "Enter your name: "
+# # name = gets
+# name = gets.chomp()
+# puts "Enter your age: "
+# age = gets.chomp()
+# puts ('Hello ' + name + ", You are " + age + " years old")
+
+# #################################################################
+# ##################### Building a calculator In Ruby #############
+# #################################################################
+
+# puts 'Enter a number: '
+# num1 = gets.chomp().to_f
+# puts 'Enter another number: '
+# num2 = gets.chomp().to_f
+
+# puts (num1.to_i + num2.to_i)
+# puts (num1.to_f + num2.to_f)
+# puts (num1 + num2)
+
+# #################################################################
+# ##################### Building a Mad Libs Game In Ruby ##########
+# #################################################################
+
+# puts 'Enter a color: '
+# color = gets.chomp()
+# puts 'Enter a plural noun: '
+# plural_noun = gets.chomp()
+# puts 'Enter a celebrity: '
+# celebrity = gets.chomp()
+
+# puts ('Roses are ' + color)
+# puts (plural_noun + ' are blue')
+# puts ('I love ' + celebrity)
+
+# #################################################################
+# ##################### Array In Ruby #############################
+# #################################################################
+
+friends = Array['Simon', 'Jimon', 'Takmina', 30, 28, 25, false, true]
+# puts friends
+# puts friends[0]
+# puts friends[-1]
+# puts friends[0, 3]
+
+# friends[0] = 'Arman'
+# puts friends[0]
+
+# people = Array.new
+# people[0] = 'John'
+# puts people[0]
+
+# puts friends.length()
+# puts friends.include? "Jaman"
+# puts friends.reverse()
+# puts friends.sort()
+
+# #################################################################
+# ##################### Hashes In Ruby #############################
+# #################################################################
