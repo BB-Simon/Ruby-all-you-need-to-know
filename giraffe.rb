bcab2fca6507326623cfd0db9841e6f53f106438
BB-Simon <simon.chowdery@gmail.com>
2023-01-09 21:25:53 +0200

    Case expression in ruby

--- giraffe.rb
+++ giraffe.rb
@@ -237,3 +237,32 @@ max_num = max(10, 15, 9)
 #   puts 'Invalid operator!'
 # end
 
+# #################################################################
+# ##################### Case Expression In Ruby #########
+# #################################################################
+
+def get_day_name(day)
+  day_name = ''
+
+  case day
+  when "mon"
+    day_name = "Monday"
+  when "tue"
+    day_name = "Tuesday"
+  when "wed"
+    day_name = "Wednesday"
+  when "thu"
+    day_name = "Thursday"
+  when "fri"
+    day_name = "Friday"
+  when "sat"
+    day_name = "Saterday"
+  when "sun"
+    day_name = "Sunday"
+  else
+    day_name = "Invalid abbribeation"
+  end
+  return day_name
+end
+
+# puts get_day_name('fri')
